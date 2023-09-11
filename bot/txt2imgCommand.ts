eb3950b79cdc98554f4641157f32cad71cb1c007
pinks <lisq@cock.li>
2023-09-11 11:22:15 +0200

    fix: not awaiting

--- bot/txt2imgCommand.ts
+++ bot/txt2imgCommand.ts
@@ -18,26 +18,30 @@ export async function txt2imgCommand(ctx: Grammy.CommandContext<Context>) {
 
 async function txt2img(ctx: Context, match: string, includeRepliedTo: boolean): Promise<void> {
   if (!ctx.message?.from?.id) {
-    return void ctx.reply("I don't know who you are");
+    await ctx.reply("I don't know who you are");
+    return;
   }
 
   const config = ctx.session.global;
   if (config.pausedReason != null) {
-    return void ctx.reply(`I'm paused: ${config.pausedReason || "No reason given"}`);
+    await ctx.reply(`I'm paused: ${config.pausedReason || "No reason given"}`);
+    return;
   }
 
   const jobs = await jobStore.getBy("status.type", "waiting");
   if (jobs.length >= config.maxJobs) {
-    return void ctx.reply(
+    await ctx.reply(
       `The queue is full. Try again later. (Max queue size: ${config.maxJobs})`,
     );
+    return;
   }
 
   const userJobs = jobs.filter((job) => job.value.request.from.id === ctx.message?.from?.id);
   if (userJobs.length >= config.maxUserJobs) {
-    return void ctx.reply(
+    await ctx.reply(
       `You already have ${config.maxUserJobs} jobs in queue. Try again later.`,
     );
+    return;
   }
 
   let params = parsePngInfo(match);
@@ -52,11 +56,12 @@ async function txt2img(ctx: Context, match: string, includeRepliedTo: boolean): 
     };
   }
   if (!params.prompt) {
-    return void ctx.reply(
+    await ctx.reply(
       "Please tell me what you want to see." +
         txt2imgQuestion.messageSuffixMarkdown(),
       { reply_markup: { force_reply: true, selective: true }, parse_mode: "Markdown" },
     );
+    return;
   }
 
   const replyMessage = await ctx.reply("Accepted. You are now in queue.");
