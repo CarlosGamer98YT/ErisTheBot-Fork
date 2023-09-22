10b57926ed18802a6390c997f395236d3d1b5031
pinks <lisq@cock.li>
2023-09-23 01:40:21 +0200

    fix: cancel command

--- bot/cancelCommand.ts
+++ bot/cancelCommand.ts
@@ -4,7 +4,7 @@ import { Context } from "./mod.ts";
 export async function cancelCommand(ctx: Context) {
   const jobs = await generationQueue.getAllJobs();
   const userJobs = jobs
-    .filter((job) => job.lockUntil > new Date())
+    .filter((job) => job.lockUntil < new Date())
     .filter((j) => j.state.from.id === ctx.from?.id);
   for (const job of userJobs) await generationQueue.deleteJob(job.id);
   await ctx.reply(`Cancelled ${userJobs.length} jobs`);
